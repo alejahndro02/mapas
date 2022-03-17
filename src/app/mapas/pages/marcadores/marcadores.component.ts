dd38db0a71b859b2cb5b505eacef30cbbb52c3b5
alejahndro02 <alejahndro02@gmail.com>
2022-03-17 17:59:14 -0600

    se declaro el decorador viewChield y las propiedades de mapa zoomLevel  center, tambien se declaro el ciclo de vida de ngAfterViewInit donde se inicializa el mapa por ultimo se creo un marcador

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import    { AfterViewInit, 
+            Component, 
+            ElementRef,
+            ViewChild      } from '@angular/core';
+import * as mapboxgl         from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+      .mapa-container{
+      height:100%;
+      width:100%
+    }
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!  : ElementRef;
+  mapa!     : mapboxgl.Map;
+  zoomLevel : number = 16;
+  center    : [number, number] = [-98.76848261905333, 19.127491747406925]
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+
+    this.mapa = new mapboxgl.Map({
+      container:this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center:this.center,
+      zoom:this.zoomLevel
+      
+    });
+    
+    /* otra forma de crear un marcador es creando un div pero en lugar de poner un marcador tipo 
+     pin pone un marcador tipo texto */
+    const markerHtml: HTMLElement = document.createElement('div');
+    markerHtml.innerHTML='hola componente';
+
+    // Se crea una instacia nueva para crear un marcador 
+    const marker = new mapboxgl.Marker({element:markerHtml})
+    .setLngLat(this.center)
+    .addTo(this.mapa);
   }
 
 }
