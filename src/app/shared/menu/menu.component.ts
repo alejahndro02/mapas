d9449aa4eef9fd3ba99284d0e517afa55d75c7b8
alejahndro02 <alejahndro02@gmail.com>
2022-03-17 08:50:44 -0600

    Se declaro una interface y se agrego un arreglo de item que tinen las rutas y el nombre

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+ interface MenuItem{
+   ruta:string,
+   nombre:string
+ }
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    `li{
+      cursor:pointer
+    }`
   ]
 })
 export class MenuComponent implements OnInit {
-
+  menuItem:MenuItem[]=[
+    {
+      ruta:'./mapas/full-screen',
+      nombre:'full-screen'
+    },
+    {
+      ruta:'./mapas/marcadores',
+      nombre:'marcadores'
+    },
+    {
+      ruta:'./mapas/propiedades',
+      nombre:'propiedades'
+    },
+    {
+      ruta:'./mapas/zoom-range',
+      nombre:'zoom-range'
+    },
+  ]
   constructor() { }
 
   ngOnInit(): void {
