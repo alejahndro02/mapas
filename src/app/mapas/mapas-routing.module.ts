3efeb1e237ed565ee710f85aca67d360be5c595d
alejahndro02 <alejahndro02@gmail.com>
2022-03-17 08:30:52 -0600

    se crearon las rutas hijas

--- src/app/mapas/mapas-routing.module.ts
+++ src/app/mapas/mapas-routing.module.ts
@@ -1,7 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule             } from '@angular/core';
+import { RouterModule,
+         Routes               } from '@angular/router';
+import { FullScreenComponent  } from './pages/full-screen/full-screen.component';
+import { MarcadoresComponent  } from './pages/marcadores/marcadores.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
+import { ZoomRangeComponent   } from './pages/zoom-range/zoom-range.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',
+    children:[
+      { path:'full-screen',
+        component: FullScreenComponent
+      },
+      { path:'marcadores',
+        component: MarcadoresComponent
+      },
+      { path:'propiedades',
+        component: PropiedadesComponent
+      },
+      { path:'zoom-range',
+        component: ZoomRangeComponent
+      },
+      {
+        path:'**',
+        redirectTo: 'full-screen'
+      }
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
