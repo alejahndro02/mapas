6ff5aa7510955fcde8155c8ce42811e57e060ce0
alejahndro02 <alejahndro02@gmail.com>
2022-03-17 11:31:37 -0600

    se importo la libreria de mapBox,  agregaron los estilos para el componente y se declaro la variable map

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+    .mapa-container{
+      height:100%;
+      width:100%
+    }
+    .row{
+      color:red;
+      background-color:white;
+      border-radius:5px;
+      bottom:50px;
+      left:50px;
+      padding:10px;
+      position:fixed;
+      z-index:99999;
+    }
+    `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +28,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container:'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center:[-98.76848261905333, 19.127491747406925],
+      zoom:16
+    })
   }
 
 }
