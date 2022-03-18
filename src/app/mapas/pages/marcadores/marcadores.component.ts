296f923992d15de8561f0e0fb0f384b58043e277
alejahndro02 <alejahndro02@gmail.com>
2022-03-17 21:29:49 -0600

    Se declaro una constante color que proporcionara un color random al ser creada y tambien se declaro una contante que contiene el nuevomarcador creado

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -9,11 +9,20 @@ import * as mapboxgl         from 'mapbox-gl';
   templateUrl: './marcadores.component.html',
   styles: [
     `
-      .mapa-container{
+    .mapa-container{
       height:100%;
       width:100%
     }
-    `
+    .list-group{
+      position:fixed;
+      top:20px;
+      right: 20px;
+      z-index:99;
+    }
+    li{
+      cursor:pointer;
+    }
+  `
   ]
 })
 export class MarcadoresComponent implements AfterViewInit {
@@ -34,18 +43,35 @@ export class MarcadoresComponent implements AfterViewInit {
       zoom:this.zoomLevel
 
     });
+    // marcadores Dinamicos
     
+
+
+    //  marcadodes estaticos que se inicializan cuando se inicailiza el componente 
+
     /* otra forma de crear un marcador es creando un div pero en lugar de poner un marcador tipo 
      pin pone un marcador tipo texto */
     //  Marcador perzonalizadoo
-    const markerHtml: HTMLElement = document.createElement('div');
-    markerHtml.innerHTML='hola componente';
+    /*const markerHtml: HTMLElement = document.createElement('div');
+    markerHtml.innerHTML='hola componente';*/
 
     // Se crea una instacia nueva para crear un marcador
     // marcador por defecto  
-    const marker = new mapboxgl.Marker({element:markerHtml})
+    /*const marker = new mapboxgl.Marker({element:markerHtml})
     .setLngLat(this.center)
-    .addTo(this.mapa);
+    .addTo(this.mapa);*/
   }
-
+  agregarMarcador(){
+    // Crea un color aleatorio
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    
+    // Crea un marcador nuevo
+    const nuevoMarcador= new mapboxgl.Marker({
+      draggable:true,
+      color
+    })
+      .setLngLat(this.center)
+      .addTo(this.mapa)
+  }
+  irMarcador(){}
 }
