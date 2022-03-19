8d3a490eb1ad14b6a2485f341a14931715052a4e
alejahndro02 <alejahndro02@gmail.com>
2022-03-19 12:06:31 -0600

    se creo una interface de tipo Propiedad y se declaro una pripiedad llamada porpiedades que contiene un array de objetos

--- src/app/mapas/pages/propiedades/propiedades.component.ts
+++ src/app/mapas/pages/propiedades/propiedades.component.ts
@@ -1,12 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Propiedad {
+  titulo: string;
+  descripcion: string;
+  lngLat: [number, number];
+}
 @Component({
   selector: 'app-propiedades',
   templateUrl: './propiedades.component.html',
   styles: [
   ]
 })
+
 export class PropiedadesComponent implements OnInit {
+  urlImg:string= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCo1XSPI04nO2-FLGABd5VOqz_1NrK_jcqOWXD5Oyby_WyY8tN0kQ4Y5jjOOyocVlIzYw&usqp=CAU' 
+  // Decalracion de la data
+  propiedades: Propiedad[] = [
+    {
+      titulo: 'Casa residencial, Canadá',
+      descripcion: 'Bella propiedad en Katana, Canadá',
+      lngLat: [ -75.92722289474008, 45.280015511264466]
+    },
+    {
+      titulo: 'Casa de playa, México',
+      descripcion: 'Hermosa casa de playa en Acapulco, México',
+      lngLat: [ -99.91287720907991, 16.828940930185748]
+    },
+    {
+      titulo: 'Apartamento, Argentina',
+      descripcion: 'Lujoso apartamento en el corazón de Buenos Aires, Argentina',
+      lngLat: [ -58.430166677283445, -34.57150108832866 ]
+    },
+    {
+      titulo: 'Local comercial, España',
+      descripcion: 'Local comercial disponible en Madrid, España, cerca de El Jardín Secreto.',
+      lngLat: [ -3.7112735618380177, 40.42567285425766 ]
+    },
+  ]
 
   constructor() { }
 
